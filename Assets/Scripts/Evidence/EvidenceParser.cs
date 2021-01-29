efd79d9569a0e5cc19e9286103bd93ced71f4013
marta_vitos <martavitos20@gmail.com>
2021-01-29 16:56:06 +0200

    Text mesh pro + evidence parser. Prefabs for desk are coming soon

---
+++ Assets/Scripts/Evidence/EvidenceParser.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EvidenceParser : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI partialDescriptionText;
+    [SerializeField] TextMeshProUGUI fullDescriptionText;
+    [SerializeField] Image imageHolder;
+    [SerializeField] Evidence evidenceInfo;
+
+    private void Start()
+    {
+        ParseInfo();
+    }
+
+    private void ParseInfo()
+    {
+        partialDescriptionText.text = evidenceInfo.partialDescription;
+        fullDescriptionText.text = evidenceInfo.fullDescription;
+        imageHolder.sprite = evidenceInfo.image;
+    }
+}
