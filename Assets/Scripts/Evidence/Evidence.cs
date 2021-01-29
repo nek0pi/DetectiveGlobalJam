8f0bc1a4f2f23c8e4a1f9e6b1e0b146211bd5eba
marta_vitos <martavitos20@gmail.com>
2021-01-29 15:22:25 +0200

    Evidence

---
+++ Assets/Scripts/Evidence/Evidence.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="EvidenceData", menuName = "ScriptableObjects/Create new evidence data", order = 1)]
+public class Evidence : ScriptableObject
+{
+    public int id;
+    public string partialDescription;
+    public string fullDescription;
+    public Sprite image;
+    public EvidenceType type;
+}
+
+public enum EvidenceType
+{ 
+    Crutial, Secondary, Misleading 
+}
--- Assets/Scripts/Interactive.cs
+++ Assets/Scripts/Interactive.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Interactive : MonoBehaviour
+public abstract class Interactive : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Update()
     {
-        
+        //We need to get from Input Manager data of what User have clicked. 
+        //Cast Ray from mouse position and check if something had been hitted and if it is our layer.
+        //We will remove (or not) this part later when Input manager are done.
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Evidence"))
+                {
+                    Debug.Log("Yes");
+                    //Call here method from dialogue controller
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void CallDialogue(int index);
+
 }
