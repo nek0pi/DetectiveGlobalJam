d8aaa44ef5b024868c8eaf89f5ec552604d5ca88
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-29 17:30:58 +0300

    add PlayerController

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : StateMachine
+{
+    [HideInInspector]
+    public MovementController MovementController;
+
+    private void Awake()
+    {
+        MovementController = GetComponent<MovementController>();
+        currentState = new PlayerNormalState(this);
+    }
+
+    private void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Move(input);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+
+    private void Move(Vector2 input)
+    {
+        if (currentState != null)
+            StartCoroutine(currentState.Move(input));
+    }
+}
