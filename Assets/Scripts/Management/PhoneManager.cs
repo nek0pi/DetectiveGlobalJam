273ea4bd17a8f36b12af2000c1fc33fb71448032
scorpzor <scorpzor.art@gmail.com>
2021-01-30 23:22:10 +0200

    Phone manager

---
+++ Assets/Scripts/Management/PhoneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PhoneManager : Singleton<PhoneManager>
+{
+    [SerializeField] private GameObject phoneScreen;
+    public Animator anim;
+    private void Start()
+    {
+        phoneScreen.SetActive(true);
+        //todo: change to false and call OpenPhone() from app
+    }
+
+    public void OpenPhone()
+    {
+        phoneScreen.SetActive(true);
+        anim.SetBool("isOpened", true);
+    }
+
+    public void ClosePhone()
+    {
+        anim.SetBool("isOpened", false);
+        StartCoroutine("WaitAndClose");
+    }
+
+    private IEnumerator WaitAndClose()
+    {
+        yield return new WaitForSeconds(1.15f);
+        phoneScreen.SetActive(false);
+    }
+}
