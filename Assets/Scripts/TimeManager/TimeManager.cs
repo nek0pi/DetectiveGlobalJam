f4063d81e5fc9f55c20ff1df5bfeb5766817f86e
scorpzor <scorpzor.art@gmail.com>
2021-01-30 23:22:10 +0200

    Phone manager
    (cherry picked from commit 273ea4bd17a8f36b12af2000c1fc33fb71448032)

--- Assets/Scripts/TimeManager/TimeManager.cs
+++ Assets/Scripts/TimeManager/TimeManager.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class TimeManager : Singleton<TimeManager>
 {
-    public float timeRemaining { get; set; }
+    public float timeRemaining { get; set; } = 21600f;
     public Action onTimeOut;
     public TextMeshProUGUI textUI;
 
+    private void Start()
+    {
+        UpdateUI();
+    }
+    
     public void ChangeTime(float time)
     {
         timeRemaining -= time;
-        Mathf.Clamp(timeRemaining, 0, 100);
+        Mathf.Clamp(timeRemaining, 0, int.MaxValue);
         if (timeRemaining == 0)
         {
             onTimeOut?.Invoke();
@@ -22,6 +27,7 @@ public class TimeManager : Singleton<TimeManager>
 
     private void UpdateUI()
     {
-        textUI.text = timeRemaining.ToString("00");
+        var t = TimeSpan.FromSeconds(timeRemaining);
+        textUI.text = $"{t.Hours:00}:{t.Minutes:00}";
     }
 }
