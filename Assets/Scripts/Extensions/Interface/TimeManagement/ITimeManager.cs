bcfe096ebad0ca59eed9d85c3ffbd3e7cb24a08b
scorpzor <scorpzor.art@gmail.com>
2021-01-30 02:07:04 +0200

    TimeManager

---
+++ Assets/Scripts/Extensions/Interface/TimeManagement/ITimeManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Extensions.Interface.TimeManagement
+{
+    public interface ITimeManager
+    {
+        public float timeRemaining { get; set; }
+        public bool timeExpired { get; set; }
+        public void ChangeTime(float time);
+    }
+}
