4fe18038a39506d577ca1cd16883fc5c23015097
scorpzor <scorpzor.art@gmail.com>
2021-01-30 18:45:44 +0200

    PauseScreen

--- Assets/Scripts/Management/HintManager.cs
+++ Assets/Scripts/Management/HintManager.cs
@@ -1,28 +1,25 @@
 ﻿using System.Linq;
 
-namespace Management
+public class HintManager : Singleton<HintManager>
 {
-    public class HintManager : Singleton<HintManager>
-    {
-        private int i;
-        private readonly ProgressManager p = ProgressManager.Instance;
+    private int i;
+    private readonly ProgressManager p = ProgressManager.Instance;
 
-        public void GetHint()
+    public void GetHint()
+    {
+        var ec = p.CriticalEvidence.Count;
+        if (ec == 0)
         {
-            var ec = p.CriticalEvidence.Count;
-            if (ec == 0)
-            {
-                return;
-            }
-            if (i == ec-1)
-            {
-                i = 0;
-            }
-            var evidenceList = p.CriticalEvidence.Select(
-                (value, index) => new {value.id, index});
-            var evidence = evidenceList.Where(e => e.index == i).Select(e=>e.id).FirstOrDefault();
-            i++;
-            //todo: call dialog trigger with [var evidance]
+            return;
         }
+        if (i == ec-1)
+        {
+            i = 0;
+        }
+        var evidenceList = p.CriticalEvidence.Select(
+            (value, index) => new {value.id, index});
+        var evidence = evidenceList.Where(e => e.index == i).Select(e=>e.id).FirstOrDefault();
+        i++;
+        //todo: call dialog trigger with [var evidance]
     }
 }
--- Assets/Scripts/Management/PauseManager.cs
+++ Assets/Scripts/Management/PauseManager.cs
@@ -1,1 +1,60 @@
-// Causes game to pause and triggers pause UI
+using UnityEngine;
+
+public class PauseManager : Singleton<PauseManager>
+{
+    [SerializeField] private GameObject pauseScreen;
+    public static bool gameIsPaused;
+    
+    void Start()
+    {
+        pauseScreen.SetActive(gameIsPaused);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+    
+    public void PauseGame()
+    {
+        gameIsPaused = !gameIsPaused;
+        if(gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            pauseScreen.SetActive(!gameIsPaused);
+        }
+        else 
+        {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(!gameIsPaused);
+        }
+    }
+
+    public void LoadMenuScreen()
+    {
+        Debug.Log("LoadMenuScreen.Call");
+        PauseGame();
+        //todo: screen manager to main menu here
+    }
+
+    public void SaveGame()
+    {
+        Debug.Log("SaveGame.Call");
+        //todo: save manager to save game here
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("LoadGame.Call");
+        //todo: save manager to load game here
+    }
+
+    public void LoadSettings()
+    {
+        Debug.Log("LoadSettings.Call");
+        //todo: screen manager to settings menu here
+    }
+}
