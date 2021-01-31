dbc3e13419872cbe4254dbb06c38ba1fd75f251d
marta_vitos <martavitos20@gmail.com>
2021-01-31 13:05:07 +0200

    No bug when can walk while in dialog system
    (cherry picked from commit 384f79b6a05420699af0c5bab17178734945b684)

--- Assets/Scripts/Controllers/NewPlayerController.cs
+++ Assets/Scripts/Controllers/NewPlayerController.cs
@@ -14,8 +14,20 @@ public class NewPlayerController : MonoBehaviour
     {
         InputManager.Instance.allInteractive += MoveCharacter;
         InputManager.Instance.allNonIneractive += MoveCharacter;
+        DialogueManager.Instance.onDialogueStarted += ChangeStateToDialog;
+        DialogueManager.Instance.onDialogueHasFinished += ChangeStateToWalking;
         OnWalkEnd += GetInteraction;
         characterAnimator = GetComponent<Animator>();
+    }
+
+    public void ChangeStateToDialog()
+    {
+        currentPlayerState = PlayerState.Talking;
+    }
+
+    public void ChangeStateToWalking()
+    {
+        currentPlayerState = PlayerState.Moving;
     }
 
     public void MoveCharacter(RaycastHit raycastHit)
